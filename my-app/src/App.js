e588ea858846f0d3769e07b9cc34071021fda15c
quan <dinhhonquan1907@gmail.com>
2022-11-03 20:55:19 +0700

    update front-end

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import Main from './Year3/Main';
+// import Main from './Year3/Main';
 import Navbar from './Year3/Navbar';
 import Footer from './Year3/Footer';
 
@@ -12,25 +12,9 @@ function App() {
           <Navbar/>
         </section>
 
-        <div className='container px-2 py-2'>
-          <div className='row'>
-            <div className='col-3'>
-              <div className='row mx-1 my-1 rounded text-center border bg-light sub-content'><Main/></div> 
-              <div className='row mx-1 my-1 rounded text-center border bg-light sub-content'><Main/></div>
-              <div className='row mx-1 my-1 rounded text-center border bg-light sub-content'><Main/></div>
-            </div>
-
-            <div className='col-9'>
-              <div className='row mx-1 my-1 rounded text-center border bg-light sub-content'><Main/></div>
-              <div className='row mx-1 my-1 rounded text-center border bg-light sub-content'><Main/></div>
-              <div className='row mx-1 my-1 rounded text-center border bg-light sub-content'><Main/></div>
-              <div className='row mx-1 my-1 rounded text-center border bg-light sub-content'><Main/></div>
-              <div className='row mx-1 my-1 rounded text-center border bg-light sub-content'><Main/></div>
-              
-            </div>
-
-          </div>
-        </div>
+        {/* <section>
+          <Main/>
+        </section> */}
 
         <section>
           <Footer/>
